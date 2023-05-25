2bf8df6179171712ae1cdf09042f222c3b20ee4c
Yudai-creator <yudaipx77@gmail.com>
2023-05-25 15:51:44 -0400

    perf:tailwind css installed, trying vuesax

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
     <NuxtWelcome />
+    <vs-button>
+        Hello World Vuesax + Nuxtjs
+    </vs-button>
   </div>
 </template>
+
+<script setup>
+
+// import Vuesax from 'vuesax3'
+// import 'vuesax3/dist/vuesax.css' //Vuesax styles
+// import { vsButton, vsSelect, vsPopup } from 'vuesax3'
+
+useHead({
+  script: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/vuesax/dist/vuesax.umd.js'
+    }
+  ],
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/vuesax/dist/vuesax.css'
+    }
+  ]
+})
+
+</script>
